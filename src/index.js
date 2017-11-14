7b4534f2b659c50b03e0d78049e7ccff8c2bdbd8
Andrei Ausianik <andrei.ausianik@iii.com>
2017-11-14 15:35:31 +0300

    points on a line task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function isPointsOnLine(points) {
-  // your solution here
+  points = points.slice();
+  const [[x1, y1], [x2, y2]] = points.splice(0, 2);
+  const a = (y2 - y1) / (x2 - x1);
+  const b = y1 - a * x1;
+  return points.every(([x, y]) => isFinite(a) ? a * x + b == y : x === x1);
 }
